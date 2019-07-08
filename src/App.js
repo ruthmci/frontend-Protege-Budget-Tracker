fb691caf845445b40b161666f3f4c4a665187b82
ewhiley <31295147+ewhiley@users.noreply.github.com>
2019-07-08 15:18:33 +1000

    Add files via upload
    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  
+  render() {
+    return(
+      <h1>Budget tracker app</h1>
+    )
+  }
+    
+}
+
+export default App;
