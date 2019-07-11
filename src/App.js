96f7508a1f9bc663a70209052dc9e0f4a331bafc
ewhiley <ejwhiley29@gmail.com>
2019-07-11 15:26:03 +1000

    calculating expenditure

--- src/App.js
+++ src/App.js
@@ -5,23 +5,22 @@ import axios from 'axios'
 
 class App extends React.Component {
   state = {
-    proteges: [],
-    items: []
+    proteges: []
+    // items: []
   }
   
   async componentDidMount() {
     const getProteges = 'http://localhost:5000/proteges'
-    const getItems = 'http://localhost:5000/items'
+    // const getItems = 'http://localhost:5000/items'
     
     const protegeResponse = await fetch(getProteges)
     const protegeData = await protegeResponse.json()
     
-    const itemResponse = await fetch(getItems)
-    const itemData = await itemResponse.json()
+    // const itemResponse = await fetch(getItems)
+    // const itemData = await itemResponse.json()
 
     this.setState({
-      proteges: protegeData,
-      items: itemData      
+      proteges: protegeData   
     })
   }
 
--- src/components/ProtegeView.js
+++ src/components/ProtegeView.js
@@ -6,23 +6,29 @@ const renderItems = (items) => {
       <div key={index}>
         {/* <h2>{item.protegename}</h2> */}
         <ul>
-          <li> {item.description} | $ {item.amount}</li>
+          <li> {item.description} | $ {item.expenditure}</li>
         </ul>
       </div>
     )
   })
 } 
 
- 
+// meals.reduce((totalCalories, meal) => totalCalories + meal.calorie, 0)
+
+// this.state.meals.reduce((accumulator, currentValue)=> accumulator + accumulator, currentValue.calorie,0);
+
+const convertedtoInt = (items) => items.map((item, index) => parseInt(item.expenditure, 10))
+
+const calculateExpenditure = (convertedtoInt) => items.reduce((total, item) => total.expenditure + item.expenditure, 0)
 
 const ProtegeView = (props) => {
   console.log(props)
   return (
     <>
-      <h1>Name: {props.protege.protegename}</h1>
-      <h2>Items purchased: {renderItems(props.items)}</h2>
-      <h2>Total spent: {props.protege.expenditure}</h2>
-      <h2>Balance: {props.protege.balance}</h2>
+      <h1>Name: {props.protege.protege.protegename}</h1>
+      <h2>Items purchased: {renderItems(props.protege.items)}</h2>
+      <h2>Total spent: {calculateExpenditure(props.protege.items)}</h2>
+      <h2>Balance: {props.protege.protege.balance}</h2>
     </>
   )
 }
